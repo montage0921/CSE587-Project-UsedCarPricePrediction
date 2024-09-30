991f38908943738a1ebd5f5f03e4f13f63122daf
unknown <garyshi0921@gmail.com>
2024-09-30 13:19:16 -0400

    car_info scraping finish

--- Car_Info.py
+++ Car_Info.py
@@ -2,11 +2,14 @@ from selectolax.parser import HTMLParser
 from playwright.sync_api import sync_playwright
 import requests as r
 import time
+import logging
+import pandas as pd
 
 class Car_Info:
     def __init__(self,keyword,pages):
-        self.keyword=keyword # "honda","sedan","red+car"(please use + concat each word)...
+        self.keyword=keyword # "honda","sedan","red+car"(please use + to concat each word)...
         self.pages=pages # 22 pcs of info per page
+        self.car_data=[]
     
     # get search page for a specific keyword
     def _get_html(self):
@@ -21,7 +24,7 @@ class Car_Info:
             page.wait_for_load_state("networkidle", timeout=TIMEOUT)
 
             counter=1 
-            total_page=self.pages # how many pages we would like to open. 22 items/page
+            total_page=self.pages # how many pages we would like to open. 23 items/page
             while counter<=total_page:
                 try:
                     print("See More Matches!")
@@ -46,7 +49,7 @@ class Car_Info:
             return page.inner_html("body") 
     
    
-    def get_links(self):
+    def _get_links(self):
         # pages: # of pages we want to scrape
 
         html=self._get_html()
@@ -55,7 +58,69 @@ class Car_Info:
         links=[i.attributes['href'] for i in link_tags if 'href' in i.attributes]
         return links
 
+    def scrape_car_info(self):
 
-honda=Car_Info("honda",5)
-links=honda.get_links()
-print(links)
+        links=self._get_links()
+        print(links)
+        print(len(links)) 
+
+        for link in links:
+            url=f"https://www.carmax.com{link}"
+
+            with sync_playwright() as p:
+                browser = p.chromium.launch(headless=False) 
+                page = browser.new_page()
+                page.goto(url) 
+                
+                try:
+                    page.wait_for_load_state("networkidle", timeout=9000)
+                except TimeoutError:
+                    logging.error(f"{link} loading takes too long...")
+                    continue
+
+                html=page.inner_html("body")
+                tree=HTMLParser(html)
+
+                car={} # save one car's info
+
+                # extract year, make and model
+                description=tree.css_first("h1#car-header-basic-car-info").text()
+                descr_list=description.split(" ") 
+                year=descr_list[0]
+                make=descr_list[1]
+                model=' '.join(descr_list[2:])
+
+                car["year"]=year
+                car["make"]=make
+                car["model"]=model
+
+                # extract price
+                price_literal=tree.css_first("span#default-price-display").text()
+                price=int(price_literal.replace('$','').replace(',','').replace('*',''))
+                car["price"]=price
+                # extract mileage
+                mileage_literal=tree.css_first("span.car-header-mileage").text()
+                mileage=int(mileage_literal.split(" ")[0].replace('K',''))*1000
+                car["mileage"]=mileage
+
+                # extract motor, drive type and color
+                badges=tree.css("div.tombstone-badge")
+                for badge in badges:
+                    sub_label=badge.css_first('div.tombstone-badge-sub')
+                    if sub_label:
+                        feature_name=sub_label.text()
+                        feature_value=badge.text().replace(feature_name,"").strip()
+                        car[feature_name]=feature_value
+                
+                # append car data to the data list
+                self.car_data.append(car)
+
+    def get_car_data(self):
+        df=pd.DataFrame(self.car_data)
+        return df
+
+honda=Car_Info("tesla",10)
+honda.scrape_car_info()
+data=honda.get_car_data()
+print(data)
+

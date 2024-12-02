a2de66bc420b4dea0723ea7bedf8ec10e8b5c13a
unknown <garyshi0921@gmail.com>
2024-12-01 19:41:33 -0500

    update to fit the new csv

--- main.py
+++ main.py
@@ -11,21 +11,22 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 
 # -------------- Constant ----------------------
-kBest=10
+
 
 # read data from csv
 @st.cache_data
 def get_data():
+
     df=pd.read_csv("carinfo_after_pre_clean.csv")
     return df
 
 # data cleaning
 @st.cache_data
 def clean_data(df,list=None):
-    df_cleaned_1 = df.dropna(subset = ['exterior_color', 'interior_color', 'fuel','Drive type'])
+    df_cleaned_1 = df.dropna(subset = ['exterior_color', 'interior_color', 'fuel','drive_type'])
     df_cleaned = df_cleaned_1[df_cleaned_1['price'] != 0]
     df_unique = df_cleaned.drop_duplicates(subset = 'VIN', keep = False)
-    df_unique = df_unique.drop(columns = ['Unnamed: 0'])
+    # df_unique = df_unique.drop(columns = ['Unnamed: 0'])
 
     if list:
         df_unique=df_unique[list]
@@ -41,7 +42,7 @@ def encoded(df):
 
 # train the best-feature selector
 @st.cache_resource(show_spinner="training KBest-Selector...")
-def trainKBestSelector(df,kBest):
+def trainKBestSelector(df):
     # split the data
     train_data, test_data = train_test_split(df, test_size = 0.2, random_state = 42)
     x_train = train_data.drop(columns = ['price'])
@@ -52,7 +53,7 @@ def trainKBestSelector(df,kBest):
     x_train_imputed = imputer.fit_transform(x_train)
 
     # create a selector to get the most kBest importatnt features
-    selector = SelectKBest(score_func = f_regression, k = min(kBest, x_train_imputed.shape[1]))
+    selector = SelectKBest(score_func = f_regression, k = min(20, x_train_imputed.shape[1]))
 
     #train the selector
     selector.fit_transform(x_train_imputed, y_train)
@@ -60,15 +61,18 @@ def trainKBestSelector(df,kBest):
     return selector
 
 # get the best feature
-@st.cache_data(show_spinner=f"selecting the best {kBest} features ")
+@st.cache_data(show_spinner=f"selecting the best features ")
 def getKBestFeatures(_selector,df):
     selected_features = _selector.get_support(indices = True)
     df_without_price=df.drop(columns=['price'])
     selected_feature_names = df_without_price.columns[selected_features]
 
     # Extract the base feature names
-    base_feature_names = list(set([name.split('_')[0] for name in selected_feature_names]))
-    
+    base_feature_names = list(set([
+    '_'.join(name.split('_')[:-1]) if '_' in name else name
+    for name in selected_feature_names
+]))
+
     return base_feature_names
 
 @st.cache_resource(show_spinner="Training random forest regressor")
@@ -125,9 +129,10 @@ if __name__ == "__main__":
     df_encoded=encoded(cleaned_df) # encoding
 
     # ---------Train KBest Selector ---------
-    selector=trainKBestSelector(df_encoded,kBest)
+    selector=trainKBestSelector(df_encoded)
     important_features=getKBestFeatures(selector,df_encoded) # e.g. ["mileage", "year","color"]
     important_features.append("price")
+    st.write(important_features)
     # --------Train Random Forest Regressor ------- 
     df_important=clean_data(df,important_features)
     df_important_encoded=encoded(df_important)
@@ -140,23 +145,31 @@ if __name__ == "__main__":
     
     make=st.selectbox("Make",options=cleaned_df['make'].unique(),index=0)
     original_price=st.number_input("Original Price",min_value=5000,max_value=1000000,step=1000,value=10000)
+    st.selectbox(
+            "Model",options=cleaned_df[cleaned_df['make'] == make]['model'].unique(),
+            index=0)
     feature_input_map={
-        'mileage': lambda: st.slider("Mileage", 0, 200000, step=1000,value=30000),
-        'year':lambda:st.slider("Year", 2010, date.today().year+1, step=1,value=2020),
+        'mileage': lambda: st.number_input("Mileage", 0, 200000, step=1000,value=30000),
+        'year':lambda:st.number_input("Year", 2010, date.today().year+1, step=1,value=2020),
         'make': lambda:make,
         'model':lambda:st.selectbox(
             "Model",options=cleaned_df[cleaned_df['make'] == make]['model'].unique(),
             index=0),
-        "Drive type":lambda:st.selectbox("Drive Type",options=cleaned_df['Drive type'].unique(),index=0),
-        "owner":lambda:st.slider("Number of Owner",1,5,1),
+        "drive_type":lambda:st.selectbox("Drive Type",options=cleaned_df['drive_type'].unique(),index=0),
+        "owners":lambda:st.number_input("Number of Owner",1,5,1),
         "class":lambda:st.selectbox("Class",
                                     options=cleaned_df[cleaned_df['make'] == make]['class'].unique(),
                                     index=0),
         "cylinders":lambda:st.selectbox("Number of Cylinder",
                                        options=cleaned_df[cleaned_df['make'] == make]['cylinders'].unique(),
-                                       index=0)                    
+                                       index=0),
+        "interior_color":lambda:st.selectbox("Interior Color",options=cleaned_df[cleaned_df['make'] == make]['interior_color'].unique(),index=0),
+        "miles_per_gallon":lambda:st.selectbox("Miles Per Gallon",options=cleaned_df[cleaned_df['make'] == make]['miles_per_gallon'].unique(),index=0),
+        "fuel":lambda:st.selectbox("Fuel",options=cleaned_df[cleaned_df['make'] == make]['fuel'].unique(),index=0),
+        "bed_length":lambda:st.selectbox("Bed Length (Truck Only)",options=cleaned_df[cleaned_df['make'] == make]['bed_length'].unique(),index=0),                   
     }
 
+    
     
     with st.form(key="user_car_info"):
         st.subheader("Enter Your Car's Information!")
@@ -165,12 +178,12 @@ if __name__ == "__main__":
         columns=[col1,col2,col3]
         counter=0
         for feature in important_features:
-            if feature !='make' and feature!='price':
+            if feature !='make' and feature!='price' and feature!='model':
                 with columns[counter%3]:
                     user_input[feature]=feature_input_map[feature]()
-
-            counter+=1
+                counter+=1
         submit=st.form_submit_button("Predict",type="primary")
+        
 
     
     if submit:
@@ -179,3 +192,5 @@ if __name__ == "__main__":
             predict_price=predictByRandomForest(randomForestRegressor,user_input,df_important_encoded)
             original_price=int(original_price)
             st.metric(label="Predicted Price",value=round(predict_price,2),delta=-round(original_price-predict_price,2),delta_color="normal")
+        
+    
